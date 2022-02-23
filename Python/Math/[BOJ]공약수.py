a9a15ecd079b216152a6df3b46feca1af3a1265d
parksangmyeong1 <psm950324@naver.com>
2022-02-23 21:56:38 +0900

    [BOJ]공약수 : 5618번 문제

---
+++ Python/Math/[BOJ]공약수.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+def gcd(a, b):
+    if(a == 0):
+        return b
+
+    return gcd(b % a, a)
+
+g = gcd(arr[0], gcd(arr[1], arr[-1]))
+
+for i in range(1, (g // 2) + 1):
+    if g % i == 0:
+        print(i)
+
+print(g)
