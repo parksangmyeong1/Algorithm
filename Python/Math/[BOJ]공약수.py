25bf049dabe9641e9f7235915635f50c411e5733
parksangmyeong1 <psm950324@naver.com>
2022-02-24 20:05:04 +0900

    [BOJ]공약수 : 5618번 문제

--- Python/Math/[BOJ]공약수.py
+++ Python/Math/[BOJ]공약수.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-def gcd(a, b):
+def gcd(a, b): # 최대 공약수
     if(a == 0):
         return b
 
@@ -12,7 +12,7 @@ def gcd(a, b):
 
 g = gcd(arr[0], gcd(arr[1], arr[-1]))
 
-for i in range(1, (g // 2) + 1):
+for i in range(1, (g // 2) + 1): # 반복문 이용해서 공약수 모두 출력
     if g % i == 0:
         print(i)
 
