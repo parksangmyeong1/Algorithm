1663193f02d36caa61782a858da4422d2e72bb43
parksangmyeong1 <psm950324@naver.com>
2022-03-04 17:17:52 +0900

    [BOJ]전구 : 21918번 문제

---
+++ Python/구현/[BOJ]전구.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for i in range(m):
+    a, b, c = map(int,input().split())
+
+    if a == 1:
+        arr[b-1] = c
+
+    elif a == 2:
+        for i in range(b-1,c):
+            if arr[i] == 1:
+                arr[i] = 0
+            else: 
+                arr[i] = 1
+    
+    elif a == 3:
+        for i in range(b-1, c):
+            arr[i] = 0
+    
+    elif a == 4:
+        for i in range(b-1, c):
+            arr[i] = 1
+
+print(' '.join(map(str, arr)))
