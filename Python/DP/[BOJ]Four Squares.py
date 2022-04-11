3a3ad6db07736a60a20e69d97f8afdbb44c4a53b
parksangmyeong1 <psm950324@naver.com>
2022-04-11 22:48:06 +0900

    [BOJ]Four Squares : 17626번 문제

---
+++ Python/DP/[BOJ]Four Squares.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n + 1)
+dp[0], dp[1] = 0, 1
+
+for i in range(2, n + 1):
+    minValue = 1e9
+    j = 1
+
+    while (j ** 2) <= i:
+        minValue = min(minValue, dp[i - (j ** 2)])
+        j += 1
+    
+    dp[i] = minValue + 1
+
+print(dp[n])
